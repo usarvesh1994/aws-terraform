7adfe7989e401e2ec0da67462b4bad1f670b15ea
usarvesh-at-148000812951 <usarvesh1995@gmail.com>
2024-09-10 09:09:16 +1000

    Final Changes

--- c4-02-vpc-module.tf
+++ c4-02-vpc-module.tf
@@ -1,25 +1,22 @@
 # Create VPC Terraform Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  #version = "4.0.1"    
-  version = "5.4.0"    
+  version = "5.13.0"
 
   # VPC Basic Details
-  name = "${local.name}-${var.vpc_name}"
-  cidr = var.vpc_cidr_block
+  name            = "${local.name}-${var.vpc_name}"
+  cidr            = var.vpc_cidr_block
   azs             = var.vpc_availability_zones
   public_subnets  = var.vpc_public_subnets
-  private_subnets = var.vpc_private_subnets  
+  private_subnets = var.vpc_private_subnets
 
   # Database Subnets
-  database_subnets = var.vpc_database_subnets
-  create_database_subnet_group = var.vpc_create_database_subnet_group
+  database_subnets                   = var.vpc_database_subnets
+  create_database_subnet_group       = var.vpc_create_database_subnet_group
   create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
-  # create_database_internet_gateway_route = true
-  # create_database_nat_gateway_route = true
-  
+
   # NAT Gateways - Outbound Communication
-  enable_nat_gateway = var.vpc_enable_nat_gateway 
+  enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
 
   # VPC DNS Parameters
@@ -27,7 +24,7 @@ module "vpc" {
   enable_dns_support   = true
 
 
-  tags = local.common_tags
+  tags     = local.common_tags
   vpc_tags = local.common_tags
 
   # Additional Tags to Subnets
@@ -36,10 +33,9 @@ module "vpc" {
   }
   private_subnet_tags = {
     Type = "Private Subnets"
-  }  
+  }
   database_subnet_tags = {
     Type = "Private Database Subnets"
   }
-  # Instances launched into the Public subnet should be assigned a public IP address.
-  map_public_ip_on_launch = true
+
 }
